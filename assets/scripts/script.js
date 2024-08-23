1c3b1a7021ed4c50f815bd45b2f38485b725bd11
John <john.fcostaa@gmail.com>
2024-08-23 08:58:38 -0300

    first

---
+++ assets/scripts/script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "< FrontEnd Developer />";
+  const typewriter = document.getElementById("typewriter");
+  let index = 0;
+
+  function type() {
+    if (index < text.length) {
+      typewriter.style.width = `${index -3}ch`;
+      typewriter.textContent += text.charAt(index);
+      index++;
+      setTimeout(type, 80);
+    } else {
+      setTimeout(() => {
+        index = 0;
+        typewriter.textContent = ""; 
+        type();
+      }, 3000);
+    }
+  }
+
+  type();
+});
